399e4c8739eb8a8f96617380c13095a204d32ed8
technolog <viktor@allz.su>
2018-11-21 00:15:32 +0300

    test-sendtoaddress

--- tests/test-sendtoaddress.js
+++ tests/test-sendtoaddress.js
@@ -1,18 +1,17 @@
 const request = require('request');
 const jsonReader = require('jsonfile');
 const appConfig = jsonReader.readFileSync('../config.json'); // конфиг
-console.log(appConfig);
+
 
 request({
     method: 'post',
     json: true, // Use,If you are sending JSON data
     url: appConfig.app.apiurl + '/sendtoaddress',
-    body: {"address": "SeZLuyhhYf2qxs4ArPJ71oEu3x8EsVw51C"},
+    body: {"address": "SeZLuyhhYf2qxs4ArPJ71oEu3x8EsVw51C", "amount": 0.0005, "comment": "test rpc comment"},
     headers: {
         "Content-Type": "application/json",
         "app-Password": appConfig.app.password
     }
 }, function (err, res, data) {
     console.log(data);
-
 });
